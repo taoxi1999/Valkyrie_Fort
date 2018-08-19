95d6d1838e9bdad59e462a2c50d010e74af21223
Taoxi Jiang <naoki@Taoxis-MacBook-Pro.local>
2018-08-19 10:12:27 +0800

    Tech

--- skeleton/tests/weaponInit.py
+++ skeleton/tests/weaponInit.py
@@ -2,6 +2,6 @@
 from classes import weapon
 
 
-
+#posX, posY, name, rank, bodyPart, attack, defense, parryRate, parryEfficiency
 Training_Long_Sword = weapon(5,5, 'Training Long Sword', 1, 'TwoHanded', 10, 0, 0, 0)
 Excalibur = weapon(5,5, 'Excalibur', 8, 'TwoHanded', 100, 20, 50, 80)
